5ab542196cafd378c77d5894bb82c523c7d4c4ed
Felipe Teixeira <felipeantonioteixeira@gmail.com>
2019-04-15 00:15:09 -0300

    commit react app v2

--- qa-app/backend/src/index.js
+++ qa-app/backend/src/index.js
@@ -4,12 +4,30 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
+//Auth0 Sincronization
+const checkJwt = jwt({
+    secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://dev-w1pbqe3k.auth0.com/.well-know/jwks.json`
+  }),
+  
+  //validate user
+  audience: 'jQfSq71rJOKOTeOI4uz32FRXeMMwQlC8',
+  issuer: `https://dev-w1pbqe3k.auth0.com`,
+  algorithms: ['RS256']
+  
+  });
 
 //---------------
 const app = express();
 
 //Database
-const question = [];
+const questions = [];
 
 //Security with helmet
 app.use(helmet());
@@ -21,50 +39,53 @@ app.use(cors());
 app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
-const qs = question.map(q => ({
-id: q.id,
-title: q.title,
-description: q.description,
-answers: q.answers.length,
-}));
-
-res.send(qs);
-});
+    const qs = questions.map(q => ({
+      id: q.id,
+      title: q.title,
+      description: q.description,
+      answers: q.answers.length,
+    }));
+    res.send(qs);
+  });
 
 //----------------------
-app.get('/id',(req, res) => {
-const question = question.filter(q => (q.id === parseInt(req.params.id)));
+app.get('/:id', (req, res) => {
+const question = questions.filter(q => (q.id === parseInt(req.params.id)));
 if (question.length > 1) return res.status(500).send();
 if (question.length === 0 ) return res.status(404).send();
 res.send(question[0]);
 });
 
-//--------------------
-app.post('/', (req, res) => {
+//Post questions
+app.post('/', checkJwt, (req, res) => {
 const {title, description} = req.body;
 const newQuestion = {
-id: question.length + 1,
+id: questions.length + 1,
 title,
 description,
 answers: [],
+author: req.user.name,
 };
-question.push(newQuestion);
+questions.push(newQuestion);
 res.status(200).send();
 });
 
-//-----------------------
+//awnser to a questions
 app.post('/answer/:id', (req, res) => {
 const {answer} = req.body;
 
-const question = question.filter(q => (q.id === parseInt(req.params.id)));
+const question = questions.filter(q => (q.id === parseInt(req.params.id)));
 if (question.length > 1) return res.status(500).send();
 if (question.length === 0) return res.status(404).send();
 
 question[0].answers.push({
  answer,
+author: req.user.name,
 });
+
 res.status(200).send();
 });
+
 
 //start server
 app.listen(8080, () => {
